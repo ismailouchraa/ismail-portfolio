1d690a57309e917ca9c2d0780bd1b467d883c428
ismailouchraa <ismail.ouchraa.70@edu.uiz.ac.ma>
2026-07-29 17:45:43 +0100

    Initial portfolio setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+
+import Navbar from "../components/layout/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Welcome to my Portfolio</h1>
+      </main>
+    </>
+  );
+}
